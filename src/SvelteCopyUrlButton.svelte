371c6dbf7d2df1287551001efd196a6ac9bfb1ab
Kostadin Terziev <kostadin_terziev@abv.bg>
2020-12-01 01:40:48 +0200

    icon variable change

--- src/SvelteCopyUrlButton.svelte
+++ src/SvelteCopyUrlButton.svelte
@@ -36,7 +36,7 @@ export let timeout = 1000;
 <button style="--scu-size: {size}px" on:click={copy} class="{$$props.class}  scu-button">
   {#if !copied}
     <svg class="scu-button--icon" viewBox="0 0 16 16" version="1.1" width="{size}" height="{size}" aria-hidden="true"><path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"></path></svg>
-    {#if !icon}
+    {#if icon == false}
       <span class="scu-button--content" >{defaultText}</span>
     {/if}
   {:else}
@@ -46,7 +46,7 @@ export let timeout = 1000;
           c-9.998,9.997-9.998,26.205,0,36.203l135.994,135.992c9.994,9.997,26.214,9.99,36.204,0L504.502,111.7
           C514.5,101.703,514.499,85.494,504.502,75.496z"/>
     </svg>
-    {#if !icon}
+    {#if icon == false}
       <span class="scu-button--content">{copiedText}</span>
     {/if}
   {/if}

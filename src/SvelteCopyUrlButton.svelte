233c418015e0484aabc411c383cff1dc95d1746b
Kostadin Terziev <kostadin_terziev@abv.bg>
2020-12-01 18:58:32 +0200

    work on styles

--- src/SvelteCopyUrlButton.svelte
+++ src/SvelteCopyUrlButton.svelte
@@ -4,13 +4,13 @@ function copy() {
     alert("Please copy the URL from the location bar.");
     return;
   }
-  const dummy = document.createElement("p");
-  dummy.textContent = window.location.href;
-  document.body.appendChild(dummy);
+  const placeholder = document.createElement("p");
+  placeholder.textContent = window.location.href;
+  document.body.appendChild(placeholder);
 
   const range = document.createRange();
-  range.setStartBefore(dummy);
-  range.setEndAfter(dummy);
+  range.setStartBefore(placeholder);
+  range.setEndAfter(placeholder);
 
   const selection = window.getSelection();
   // First clear, in case the user already selected some other text
@@ -18,7 +18,7 @@ function copy() {
   selection.addRange(range);
 
   document.execCommand("copy");
-  document.body.removeChild(dummy);
+  document.body.removeChild(placeholder);
 
   copied = true;
   setTimeout(function () {
@@ -31,22 +31,17 @@ export let copiedText = "Copied!";
 export let size = 14;
 export let icon = false;
 export let timeout = 1000;
+export let styled = true;
 </script>
 
 <button
   style="--scu-size: {size}px"
   on:click="{copy}"
-  class="{$$props.class}  scu-button"
+  class="{$$props.class} scu-button"
+  class:scub-styled="{styled === true}"
 >
   {#if !copied}
-    <svg
-      class="scu-button--icon"
-      viewBox="0 0 16 16"
-      version="1.1"
-      width="{size}"
-      height="{size}"
-      aria-hidden="true"
-    ><path
+    <svg class="scu-button--icon" viewBox="0 0 16 14" aria-hidden="true"><path
         fill-rule="evenodd"
         d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"
       ></path></svg>
@@ -56,10 +51,6 @@ export let timeout = 1000;
   {:else}
     <svg
       class="scu-button--icon"
-      width="{size - 2}"
-      height="{size - 2}"
-      x="0px"
-      y="0px"
       viewBox="0 0 512 512"
       style="enable-background:new 0 0 512 512;"
       xml:space="preserve"
@@ -81,18 +72,13 @@ export let timeout = 1000;
   outline: none;
   border: none;
   background: none;
-  display: flex;
-  align-items: center;
   color: inherit;
   font-family: inherit;
   font-size: var(--scu-size);
-  margin: 0 2px;
   transition: all 120ms ease-out;
 }
-.scu-button:hover {
-  filter: brightness(2.48);
-}
 .scu-button svg {
-  margin-right: 2px;
+  width: var(--scu-size);
+  height: var(--scu-size);
 }
 </style>
